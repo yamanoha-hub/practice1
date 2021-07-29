f398dd1f6c1ae56ee1da358ae58ffe77b938fd8a
yamanoha-hab <r12m097@yahoo.co.jp>
2021-07-29 20:31:06 +0900

    gulpのタスク追加 postcss追加

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,25 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+// var sass = require('gulp-sass');
+var sass = require('gulp-sass')(require('sass'));
+var sourcemaps = require('gulp-sourcemaps');
+var plumber = require('gulp-plumber');
+var notify = require('gulp-notify');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var mqpacker = require('css-mqpacker');
 
 gulp.task('sass', function() {
   return gulp.src('./sass/**/*.scss')
+    .pipe(postcss([autoprefixer()]))
+    .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))  //エラー時に停止させずメッセージを出す。
+    .pipe(sourcemaps.init())
     .pipe(sass({outputStyle: 'expanded'}))
+    .pipe(postcss([mqpacker()]))
+    .pipe(postcss([autoprefixer()])) //ベンダープレフレックスを自動付与
+    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('./css'));
 });
 
 gulp.task('sass:watch', function() {
   gulp.watch('./sass/**/*.scss', gulp.task('sass'));
 });
-
