93cdb945630f1f05892bcea06770ef08b8770c5f
yamanoha-hab <r12m097@yahoo.co.jp>
2021-07-31 00:59:57 +0900

    autoprefixer再設定、レスポンシブ対応

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 // var sass = require('gulp-sass');
 var sass = require('gulp-sass')(require('sass'));
+var sassGlob = require('gulp-sass-glob');
 var sourcemaps = require('gulp-sourcemaps');
 var plumber = require('gulp-plumber');
 var notify = require('gulp-notify');
@@ -10,12 +11,14 @@ var mqpacker = require('css-mqpacker');
 
 gulp.task('sass', function() {
   return gulp.src('./sass/**/*.scss')
-    .pipe(postcss([autoprefixer()]))
     .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))  //エラー時に停止させずメッセージを出す。
     .pipe(sourcemaps.init())
+    .pipe(sassGlob())
     .pipe(sass({outputStyle: 'expanded'}))
     .pipe(postcss([mqpacker()]))
-    .pipe(postcss([autoprefixer()])) //ベンダープレフレックスを自動付与
+    .pipe(postcss([
+      autoprefixer()
+    ]))  //ベンダープレフレックスを自動付与
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('./css'));
 });
